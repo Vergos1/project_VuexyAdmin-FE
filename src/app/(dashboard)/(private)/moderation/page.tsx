f75e40a446b538edaa654a1d11e90cd44e26d941
Vergos1 <legendsmobajl4@gmail.com>
2024-08-28 13:26:45 +0300

    refactor: delete old code

--- src/app/(dashboard)/(private)/moderation/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContentTabs from '@/views/moderation/Tabs'
-
-export default function Page() {
-  return (
-    <div className='flex flex-col gap-6'>
-      <ContentTabs />
-    </div>
-  )
-}
